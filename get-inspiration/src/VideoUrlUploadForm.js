81804702e79233d4537a284e4dc7ee0fbd95fe88
MKim <mrn.kim07@gmail.com>
2023-11-01 13:12:03 -0700

    Adjust task video size, add loading spinner and favicon

--- get-inspiration/src/VideoUrlUploadForm.js
+++ get-inspiration/src/VideoUrlUploadForm.js
@@ -2,6 +2,7 @@ import "./VideoUrlUploadForm.css";
 import { useState, useEffect } from "react";
 import { Video } from "./Video";
 import TwelveLabsApi from "./TwelveLabsApi";
+import LoadingSpinner from "./LoadingSpinner.svg";
 
 const SERVER_BASE_URL = new URL("http://localhost:4002");
 const VIDEO_INFO_URL = new URL("/video-info", SERVER_BASE_URL);
@@ -89,6 +90,7 @@ export function VideoUrlUploadForm({
             taskStatusResponse.status === "failed"
           ) {
             setTaskVideo(null);
+            setVideoUrl(null);
             isMonitoring = false;
             fetchVideo();
           } else {
@@ -118,11 +120,14 @@ export function VideoUrlUploadForm({
       <div className="title">Upload video</div>
       <form onChange={handleChange} onSubmit={handleSubmit}>
         <input className="videoUrlUploadInput"></input>
-        <button className="videoUrlUploadButton">Upload</button>
+        <button className="videoUrlUploadButton" disabled={taskVideo}>Upload</button>
       </form>
       {taskVideo && (
         <div className="videoAndStatus">
           <Video url={taskVideo.video_url} />
+          <div className=" loading-spinner">
+          <img src={LoadingSpinner} alt="Loading Spinner" />
+        </div>
           {taskStatus ? `${taskStatus}...` : "Submitting..."}
         </div>
       )}
